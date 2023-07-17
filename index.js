9f01e78b5704aa3a9587e7edea2a2a184a0b93d4
Irina <il064330@gmail.com>
2023-07-17 21:19:20 +0300

    correct mistakes in js file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let year = document.querySelector(".year").value;
-year = Number(year);
 let button = document.querySelector(".button");
 let result = document.querySelector(".result");
 
 button.onclick = function () {
+    let year = document.querySelector(".year").value;
+year = Number(year);
 if((year % 4 == 0 && year % 100 !== 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)) {
     result.textContent = "Это високосный год";
 } else {
